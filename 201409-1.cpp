d290eda31a8f616f37971abf14161c289ff6bca4
Jhoooope <90141110+Jhoooope@users.noreply.github.com>
2022-02-26 16:37:01 +0800

    Update 201409-1.cpp

--- 201409-1.cpp
+++ 201409-1.cpp
@@ -11,10 +11,10 @@ int main()
 		cin>>x;
 		a.push_back(x);
 	}
-	sort(a.begin(),a.end());
+	sort(a.begin(),a.end());//从小到大排序
 	for(int i=1;i<n;i++)
 	{
-		if(a[i]-a[i-1]==1)
+		if(a[i]-a[i-1]==1)//若相邻两数差值为1
 		{
 			count++;
 		}
